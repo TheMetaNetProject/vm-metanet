c3f1c18b6b6fc030685a6254b901e0c8ff0f00c5
Steve Doubleday <stevedoubleday@ghmail.com>
2015-01-24 07:29:55 -0800

    diffjson.sh add test dir in path

--- diffjson.sh
+++ diffjson.sh
@@ -6,6 +6,6 @@
 # $3 directory name 
 if [ -f ~/$3/tests/Result_GS$2_m4$1_input.json ] ; then 
   /u/metanet/dev/bin/ppjson ~/$3/tests/Result_GS$2_m4$1_input.json > ~/$3/diff/temp_$1_$2.txt
-  /u/metanet/dev/bin/ppjson ~/previous-tests/Result_GS$2_m4$1_input.json > ~/$3/diff/temp_$1_$2_previous.txt
+  /u/metanet/dev/bin/ppjson ~/$3/previous-tests/Result_GS$2_m4$1_input.json > ~/$3/diff/temp_$1_$2_previous.txt
   diff ~/$3/diff/temp_$1_$2.txt ~/$3/diff/temp_$1_$2_previous.txt > ~/$3/diff/diff_$1_$2.txt
 fi
